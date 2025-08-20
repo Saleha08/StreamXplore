cddb06c564e6a0be8e01de9ce93b23adcdf021d8
Saleha Ansari <154500706+Saleha08@users.noreply.github.com>
2025-08-20 20:52:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for Engineering Information Portal
+
+// Function to toggle the mobile menu
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+}
+
